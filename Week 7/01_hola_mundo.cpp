b65673cc5154f321b3ca3278c39a5774d42e6756
JLZN1980 <jzambrano@mathrisksolution.com>
2026-05-05 13:37:43 -0500

    prueba 1

---
+++ Week 7/01_hola_mundo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int valor;
+
+    cout << "Hola, curso de Introduccion a los Lenguajes de Programacion" << endl;
+    cout << "Este archivo esta conectado con GitHub desde VS Code" << endl;
+
+    cout << "Ingresame un Valor: ";
+    cin >> valor;
+
+    cout << "El valor ingresado fue: " << valor << endl;
+
+    return 0;
+}
